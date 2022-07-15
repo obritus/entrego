a6a2d608a84eac90e58e5fff3ff8d42ed041fb3a
Kaique <caiquess4@gmail.com>
2022-07-15 15:44:02 -0300

    Atualização da Api.ts

--- mobile/Api.ts
+++ mobile/Api.ts
@@ -2,12 +2,13 @@ import axios from 'axios'
 import { useAsyncStorage } from '@react-native-async-storage/async-storage'
 
 const api = axios.create({
-	baseURL: process.env.NODE_ENV === 'development'
-	? 'http://192.168.1.75:3005/'
-	: `https://api-ux3p6ca4ia-rj.a.run.app/`
+	baseURL:
+		process.env.NODE_ENV === 'development'
+			? 'http://192.168.1.75:3005/'
+			: process.env.REACT_APP_API,
 })
 
-api.interceptors.request.use(async (config:any) => {
+api.interceptors.request.use(async (config: any) => {
 	const token = useAsyncStorage('token')
 	if (token) {
 		config.headers.Authorization = `Bearer ${token}`
@@ -23,8 +24,8 @@ interface Login {
 const Api = {
 	Login: (data: Login) => api.post('login', data),
 	Logout: (data: {}) => api.post('logout', data),
-	
-	GetEntregador: (_id:string) => api.get(`entregadores/${_id}`),
+
+	GetEntregador: (_id: string) => api.get(`entregadores/${_id}`),
 	GetEntregas: (query?: {}) => api.get('entregas', { params: query }),
 	GetEntrega: (_id: string) => api.get('entregas' + _id),
 }
